8a0f898270e783a9d70c400d4e6dd9af17605808
victor <victorphiathep@gmail.com>
2022-09-13 13:36:08 -0400

    Library work

--- problem2.py
+++ problem2.py
@@ -15,16 +15,16 @@ def one_armed_bandit():
     while game_on:
         choice = input(f"You have ${cash}. Would you like to play (y/n)? ")
         if choice.lower() == 'y':
-            spin_wheel()
+            cash += spin_wheel()
         else:
             game_on = False
 
 
     # Decrease our cash for every play and checks for no money
-    cash -= 1
-    if cash == 0:
-        print("You ran out of money.")
-        game_on = False
+        cash -= 1
+        if cash == 0:
+            print("You ran out of money.")
+            game_on = False
     
     print("Thanks for playing.")
 
@@ -32,11 +32,31 @@ def one_armed_bandit():
     # Use a while loop to take away $1 every round
 
 def spin_wheel():
-    print("Wheel is spinning!")
-    wheel_one = spin_choices[random.randint(0,6)]
-    wheel_two = spin_choices[random.randint(0,6)]
-    wheel_three = spin_choices[random.randint(0,6)]
+    print("Wheel is spinning. . .")
+
+    wheel_one = spin_choices[random.randint(0,5)]
+    wheel_two = spin_choices[random.randint(0,5)]
+    wheel_three = spin_choices[random.randint(0,5)]
     # Should return payoff amount
+
+    results = f"{wheel_one} {wheel_two} {wheel_three}"
+    print(results)
+
+    if "CHERRY" in results:
+        # Use a dictionary to give out the amount of cash depending on the amount of cherries in results
+        cherry_cash = {
+            1:2,
+            2:5,
+            3:7,
+        }
+        cherry_count = results.count('CHERRY')
+        print(f"{results} -- You win ${cherry_cash[cherry_count]}")
+        return cherry_cash[cherry_count]
+        
+
+    return 0
+
+    
 
 
 spin_choices = ['CHERRY', 'LEMON', 'ORANGE', 'PLUM', 'BELL', 'BAR']
