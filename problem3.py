c4787360903dc04213cce63a9378e94551ea6449
Victor <victorphiathep@gmail.com>
2022-09-13 16:51:33 -0400

    Problem3  start

--- problem3.py
+++ problem3.py
@@ -7,3 +7,17 @@
 
 def ampm_to_military(ampm_time):
     pass
+
+
+
+def military_to_ampm(mil_time):
+    if int(mil_time[0]) <= 12:
+        converted_time = f"{mil_time[0:1]}:{mil_time[2:3]}am"
+        return converted_time
+    else:
+        hours = int(mil_time[0:1])- 12
+        converted_time = f"{hours}:{mil_time[2:3]}pm"
+
+
+
+military_to_ampm('1212')
